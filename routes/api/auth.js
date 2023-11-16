8f27b4942c6e6d9fe4ef5652c1cd25fd3279ef83
Subash <subash.chinnannan@happiestminds.com>
2023-11-16 13:15:28 +0530

    Added user login and verification

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
 router.get("/", auth, async (req, res) => {
   try {
@@ -14,4 +18,54 @@ router.get("/", auth, async (req, res) => {
   res.send("Auth route");
 });
 
+router.post(
+  "/",
+
+  [
+    check("email", "Enter Valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+
+  async (req, res, next) => {
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+      return res.status(400).json({ errors: err.array() });
+      //   next();
+    }
+    // console.log(req.body);
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: [{ msg: "User not found" }] });
+      }
+
+      const isMismatch = await bcrypt.compare(password, user.password);
+      if (!isMismatch) {
+        return res
+          .status(400)
+          .json({ error: [{ msg: "Invalid Credentials" }] });
+      }
+
+      const payload = {
+        user: { id: user.id },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+      //   return res.send("Users route");
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).send("Server error");
+    }
+  }
+);
 module.exports = router;
